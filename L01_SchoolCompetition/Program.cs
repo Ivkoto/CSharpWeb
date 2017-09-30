615e5a9435e115f0b124335529e2a84a50c401cc
Ivaylo Kostov <ikostov87@gmail.com>
2017-09-30 11:46:30 +0300

    Add project files.

---
+++ L01_SchoolCompetition/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace L01_SchoolCompetition
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var scores = new Dictionary<string, int>();
+            var categories = new Dictionary<string, SortedSet<string>>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var args = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var name = args[0];
+                var category = args[1];
+                var points = int.Parse(args[2]);
+
+                if (!scores.ContainsKey(name))
+                {
+                    scores[name] = 0;
+                }
+                if (!categories.ContainsKey(name))
+                {
+                    categories[name] = new SortedSet<string>();
+                }
+
+                scores[name] += points;
+                categories[name].Add(category);
+            }
+
+            var orderedStudent = scores
+                .OrderByDescending(kvp => kvp.Value)
+                .ThenBy(kvp => kvp.Key);
+
+            foreach (var studentKvp in orderedStudent)
+            {
+                var name = studentKvp.Key;
+                var score = studentKvp.Value;
+                var studentCategories = categories[name];
+
+                var categoryText = $"[{string.Join(", ", studentCategories)}]";
+            }
+        }
+    }
+}
