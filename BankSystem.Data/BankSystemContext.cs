a914551def61548825c4e5c23285bdbfa5f5e980
Ivaylo Kostov <ikostov87@gmail.com>
2017-11-05 13:14:04 +0200

    override SaveChanges method in Context class

--- BankSystem.Data/BankSystemContext.cs
+++ BankSystem.Data/BankSystemContext.cs
@@ -2,6 +2,11 @@
 {
     using BankSystem.Models;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Infrastructure;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
 
     public class BankSystemContext : DbContext
     {
@@ -29,6 +34,30 @@
                 .HasMany(u => u.CheckingAccounts)
                 .WithOne(ca => ca.User)
                 .HasForeignKey(ca => ca.UserId);
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            var serviceProvider = this.GetService<IServiceProvider>();
+            var ithems = new Dictionary<object, object>();
+            foreach (var entry in this.ChangeTracker.Entries().Where(e => (e.State == EntityState.Added) || (e.State == EntityState.Modified)))
+            {
+                var entity = entry.Entity;
+                var context = new ValidationContext(entity, serviceProvider, ithems);
+                var results = new List<ValidationResult>();
+                if (Validator.TryValidateObject(entity, context, results, true))
+                {
+                    foreach (var result in results)
+                    {
+                        if (result != ValidationResult.Success)
+                        {
+                            throw new ValidationException(result.ErrorMessage);
+                        }
+                    }
+                }
+            }
+
+            return base.SaveChanges(acceptAllChangesOnSuccess);
         }
     }
 }
